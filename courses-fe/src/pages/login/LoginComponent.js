526f710a7c75c2a086ae4fa936b4d73d42dddaca
deakferi90 <deak_ferenc_jozsef@yahoo.com>
2021-11-29 12:15:52 +0200

    fixed issues regarding hooks

--- courses-fe/src/pages/login/LoginComponent.js
+++ courses-fe/src/pages/login/LoginComponent.js
@@ -1,73 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
 
-class LoginPage extends React.Component {
-    constructor(props) {
-        super(props);
+const LoginPage = (props) => {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [submitted, setSubmitted] = useState(false);
 
-        // reset login status
-
-        this.state = {
-            username: '',
-            password: '',
-            submitted: false
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(e) {
+    const handleChange = (e) => {
         const { name, value } = e.target;
-        this.setState({ [name]: value });
+        setUserName({ [name]: value });
     }
 
-    handleSubmit(e) {
+    const handleSubmit = (e) => {
         e.preventDefault();
-
-        this.setState({ submitted: true });
-        const { username, password } = this.state;
-        if (username && password) {
-            this.props.login(username, password);
+        setSubmitted(true);
+        //const { username, password } = this.state;
+        if (userName && password) {
+            this.props.login(userName, password);
         }
     }
 
-    render() {
-        const { username, password, submitted } = this.state;
-        const { increment, decrement, model } = this.props;
-        const { count } = model;
-        return (
-            <div className="login-container col-md-6 col-md-offset-3">
-                <div className='math-problem'>
-                    <div>Example for redux flow:</div>
-                    <div><button onClick={increment}>Increment</button><button onClick={decrement}>Decrement</button></div>
-                    {count}
-                </div>
-                <h2>Login</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                        <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
-                        {submitted && !username &&
-                            <div className="help-block">Username is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
-                        {submitted && !password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Login</button>
-                        <Link to="/register" className="btn btn-link">Register</Link>
-                    </div>
-                </form>
+    //const { username, password, submitted } = this.state;
+    const { increment, decrement, model } = props;
+    const { count } = model;
+    return (
+        <div className="login-container col-md-6 col-md-offset-3">
+            <div className='math-problem'>
+                <div>Example for redux flow:</div>
+                <div><button onClick={increment}>Increment</button><button onClick={decrement}>Decrement</button></div>
+                {count}
             </div>
-        );
-    }
+            <h2>Login</h2>
+            <form name="form" onSubmit={handleSubmit}>
+                <div className={'form-group' + (submitted && !userName ? ' has-error' : '')}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" className="form-control" name="username" value={userName} onChange={handleChange} />
+                    {submitted && !userName &&
+                        <div className="help-block">Username is required</div>
+                    }
+                </div>
+                <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" name="password" value={password} onChange={handleChange} />
+                    {submitted && !password &&
+                        <div className="help-block">Password is required</div>
+                    }
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary">Login</button>
+                    <Link to="/register" className="btn btn-link">Register</Link>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default LoginPage;
