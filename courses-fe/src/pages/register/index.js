45f471da8c99b24c2d72f40da980bd4108b730be
Alex Pop <alexpop3u@gmail.com>
2021-11-29 08:43:21 +0200

    added the reducer and connected it to the store

--- courses-fe/src/pages/register/index.js
+++ courses-fe/src/pages/register/index.js
@@ -1,1 +1,2 @@
 export { default as Register } from './RegisterContainer';
+export { default as duck } from './RegisterDuck';
--- courses-fe/src/store/index.js
+++ courses-fe/src/store/index.js
@@ -1,10 +1,12 @@
 import counterReducer from "./counter";
 import loggedReducer from "./isLogged";
 import { combineReducers } from "redux";
+import { duck as registerReducer } from '../pages/register';
 
 export const rootReducer = combineReducers({
     counter: counterReducer,
-    isLogged: loggedReducer
+    isLogged: loggedReducer,
+    register: registerReducer
 })
 
 export default rootReducer;
