af6d74533731bc094b713013d6180ea6995a3e99
Alex Pop <alexpop3u@gmail.com>
2021-12-04 16:00:56 +0200

    added basic logout functionality on FE

--- courses-fe/src/pages/dashboard/DashboardComponent.js
+++ courses-fe/src/pages/dashboard/DashboardComponent.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const DashboardComponent = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem('USER');
+    localStorage.removeItem('ACCESS_TOKEN');
+    navigate('/');
+  }
+
   return (
     <div>
       <h1>This is my dashboard</h1>
       <h2>This is a protected page and we will see how this goes</h2>
-      <button>This should log me out</button>
+      <button onClick={logout}>This should log me out</button>
     </div>
   );
 };
