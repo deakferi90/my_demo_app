64b644f4c9c5a39a3be3344304d26c1d09e4ee4b
Alex Pop <alexpop3u@gmail.com>
2021-12-04 18:08:21 +0200

    added mock for logout

--- courses-fe/src/pages/dashboard/DashboardComponent.js
+++ courses-fe/src/pages/dashboard/DashboardComponent.js
@@ -10,11 +10,16 @@ const DashboardComponent = () => {
     navigate('/');
   }
 
+  const logoutFromAllDevices = () => {
+    console.log('Vedem ce facem aici');
+  }
+
   return (
     <div>
       <h1>This is my dashboard</h1>
       <h2>This is a protected page and we will see how this goes</h2>
       <button onClick={logout}>This should log me out</button>
+      <button onClick={logoutFromAllDevices}></button>
     </div>
   );
 };
