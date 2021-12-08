077b6f10469d42839520373d7c3fc2372c08139a
deakferi90 <deak_ferenc_jozsef@yahoo.com>
2021-12-08 13:09:59 +0200

    added a series in Highchart

--- courses-fe/src/pages/Highchart/options.js
+++ courses-fe/src/pages/Highchart/options.js
@@ -1,6 +1,12 @@
 export const options = {
-    series: [{
-        name: 'People',
-        data: [100, 200, 300, 400, 500]
-    }]
+    series: [
+        {
+            name: 'People',
+            data: [10, 20, 30, 40, 50]
+        },
+        {
+            name: 'Persons Age',
+            data: [50, 30, 12, 40, 10]
+        },
+]
 }
