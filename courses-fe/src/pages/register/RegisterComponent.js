8048359534688539248b7e3bbe19b8ca158a5fdc
deakferi90 <deak_ferenc_jozsef@yahoo.com>
2021-12-28 13:58:19 +0200

    removed confirm password

--- courses-fe/src/pages/register/RegisterComponent.js
+++ courses-fe/src/pages/register/RegisterComponent.js
@@ -3,71 +3,61 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useRegisterCheck } from './helper';
 import './style.scss';
 
-const RegisterComponent = ({registerUser}) => {
+const RegisterComponent = ({ registerUser }) => {
     const navigate = useNavigate();
     const [submitted, setSubmitted] = useState(false);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setSubmitted(true);
-        registerUser({name, email, username, password, confirmPassword}, navigate);
+        registerUser({ name, email, username, password }, navigate);
     }
     useRegisterCheck(navigate);
 
     return (
         <form onSubmit={handleSubmit}>
-        <div className="register-container col-md-6 col-md-offset-3">
-        <h2>Register</h2>
-        <div>
-            <div className={'form-group' + (submitted && !name ? ' has-error' : '')}>
-                <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" name="name" value={name} onChange={(e) => {setName(e.target.value)}} />
-                {submitted && !name &&
-                    <div className="help-block">Username is required</div>
-                }
+            <div className="register-container col-md-6 col-md-offset-3">
+                <h2>Register</h2>
+                <div>
+                    <div className={'form-group' + (submitted && !name ? ' has-error' : '')}>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" className="form-control" name="name" value={name} onChange={(e) => { setName(e.target.value) }} />
+                        {submitted && !name &&
+                            <div className="help-block">Username is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !email ? ' has-error' : '')}>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" className="form-control" name="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                        {submitted && !email &&
+                            <div className="help-block">Email is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" name="username" value={username} onChange={(e) => { setUsername(e.target.value) }} />
+                        {submitted && !username &&
+                            <div className="help-block">Username is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" name="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                        {submitted && !password &&
+                            <div className="help-block">Password is required</div>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Register</button>
+                        <Link to="/" className="btn btn-link">Login</Link>
+                    </div>
+                </div>
             </div>
-            <div className={'form-group' + (submitted && !email ? ' has-error' : '')}>
-                <label htmlFor="email">Email</label>
-                <input type="text" className="form-control" name="email" value={email} onChange={(e) => {setEmail(e.target.value)}} />
-                {submitted && !email &&
-                    <div className="help-block">Email is required</div>
-                }
-            </div>
-            <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                <label htmlFor="username">Username</label>
-                <input type="text" className="form-control" name="username" value={username} onChange={(e) => {setUsername(e.target.value)}} />
-                {submitted && !username &&
-                    <div className="help-block">Username is required</div>
-                }
-            </div>
-            <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" name="password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
-                {submitted && !password &&
-                    <div className="help-block">Password is required</div>
-                }
-            </div>
-
-            <div className={'form-group' + (submitted && !confirmPassword ? ' has-error' : '')}>
-                <label htmlFor="password">Confirm Password</label>
-                <input type="password" className="form-control" name="confirmPassword" value={confirmPassword} onChange={(e) => {setConfirmPassword(e.target.value)}} />
-                {submitted && !confirmPassword &&
-                    <div className="help-block">Password is required</div>
-                }
-            </div>
-            
-            <div className="form-group">
-                <button className="btn btn-primary">Register</button>
-                <Link to="/" className="btn btn-link">Login</Link>
-            </div>
-        </div>
-    </div>
-    </form>
+        </form>
     );
 }
 
