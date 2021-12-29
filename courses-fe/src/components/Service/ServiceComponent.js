f81ed46d02342244cd6d37e83060e9f3a3653987
deakferi90 <deak_ferenc_jozsef@yahoo.com>
2021-12-29 15:57:30 +0200

    added an api just displayed on the page

--- courses-fe/src/components/Service/ServiceComponent.js
+++ courses-fe/src/components/Service/ServiceComponent.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import './style.scss';
+import { getData } from './api';
+
 const ServiceComponent = () => {
   const [count, setCount] = useState(0);
+  const [randomUSerDataJSON, setRandomUSerDataJSON] = useState('');
+
+  useEffect(()=> {
+    getData().then((randomData) => {
+      setRandomUSerDataJSON(randomData);
+    });
+  }, []);
 
   return (
-  <div>
-    <h2>ServicePage</h2>
-    <div>Count: {count}</div>
-    <button onClick={() => {setCount(count + 1)}}>+</button>
-    <button onClick={() => {setCount(count - 1)}} >-</button>
-  </div>
-  )}
+    <div className='display'>
+      <div>
+        <h2>ServicePage</h2>
+        <div>Count: {count}</div>
+        <button onClick={() => { setCount(count + 1) }}>+</button>
+        <button onClick={() => { setCount(count - 1) }} >-</button>
+      </div>
+      <div>
+        <p>{randomUSerDataJSON}</p>
+      </div>
+    </div>
+  )
+}
 
 export default ServiceComponent;
