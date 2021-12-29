9d5e8fcf3d097d36e94b6ea2e580237c29fe8e35
deakferi90 <deak_ferenc_jozsef@yahoo.com>
2021-12-29 16:13:23 +0200

    resolved errors

--- courses-fe/src/components/Service/api.js
+++ courses-fe/src/components/Service/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const getData = () => {
     return axios.get('https://randomuser.me/api').then(({ data }) => {
-      return JSON.stringify(data, null, 3);
+        return JSON.stringify(data, null, 3);
     }).catch((err) => {
-      console.error(err);
+        console.error(err);
     });
-  };
+};
