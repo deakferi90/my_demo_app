bca3ba96c29b3b155ac252e3065fedb20ebe9f73
deakferi90 <deak_ferenc_jozsef@yahoo.com>
2022-01-04 14:18:28 +0200

    started working on landing page restyle

--- courses-fe/src/pages/landingPage/helper.js
+++ courses-fe/src/pages/landingPage/helper.js
@@ -4,9 +4,8 @@ export const renderSlides = () =>
 names.map((person) => {
   return (
   <div>
-    <h1>{person.name}</h1>
-    <div><img className='image' alt='' src={person.src}/></div>
-    <div className='text'>{person.text}</div>
+    <div>{person.title}</div>
+    <div>{person.text}</div>
   </div>
 )});
 export default renderSlides;
